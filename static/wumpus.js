473cb4c714cd545ffc251e9de3a27b142fd25b27
Yerim Seonwoo <54367532+yerimsw@users.noreply.github.com>
2023-06-08 13:02:17 +0900

    Update: wumpus.js

--- static/wumpus.js
+++ static/wumpus.js
@@ -1,30 +1,16 @@
-function sendPostRequest(data) {
-  fetch('http://localhost:5000/wumpus', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-  .then(response => response.json())
-  .then(data => {
-    main(data);
-  });
-}
-
+/*
 fetch('http://localhost:5000/wumpus')
   .then(response => response.json())
   .then(data => {
     main(data);
 
-/* 1초마다 POST 요청을 보낸다.
+// 1초마다 POST 요청을 보낸다.
     setInterval(function() {
       sendPostRequest(data);
     }, 1000);
-*/
 
   });
-
+*/
 
 function main(data) {
 
@@ -35,7 +21,11 @@ function main(data) {
     var agentArrows = data.res.arrows;
 
     var tableContainer = document.querySelector(".table-container");
-    var table = document.getElementById("table");
+    while(tableContainer.hasChildNodes()) {
+      tableContainer.removeChild(tableContainer.firstChild);
+    }
+
+    var table = document.createElement("table");
     var agentImg = document.createElement("img");
 
     var gold = document.querySelector("#hold_gold");
@@ -54,46 +44,46 @@ function main(data) {
             // 맵의 뒷배경(wall pitch visited)
             if(agentMap[i][j][3]!=0 || agentMap[i][j][6]!=0 || agentMap[i][j][7]!=0){
                 var cellImg = document.createElement("img");
-                if(agentMap[i][j][3]==1) cellImg.src="img/wall.png";
-                else if(agentMap[i][j][6]==1) cellImg.src="img/pitch.png";
-                else if(agentMap[i][j][7]==1) cellImg.src="img/visited.png";
+                if(agentMap[i][j][3]==1) cellImg.src="wall.png";
+                else if(agentMap[i][j][6]==1) cellImg.src="pitch.png";
+                else if(agentMap[i][j][7]==1) cellImg.src="visited.png";
                 div.appendChild(cellImg);
             }
 
             // stench
             if(agentMap[i][j][0]==1) {
                 var stenchImg = document.createElement("img");
-                stenchImg.src = "img/stench.png";
+                stenchImg.src = "stench.png";
                 div.appendChild(stenchImg);
             }
 
             // breeze
             if(agentMap[i][j][1]==1) {
                 var breezeImg = document.createElement("img");
-                breezeImg.src = "img/breeze.png";
+                breezeImg.src = "breeze.png";
                 div.appendChild(breezeImg);
             }
 
             // glitter
             if(agentMap[i][j][2]==1) {
                 var glitterImg = document.createElement("img");
-                glitterImg.src = "img/glitter.png";
+                glitterImg.src = "glitter.png";
                 div.appendChild(glitterImg);
             }
 
             // wumpus
             if(agentMap[i][j][5]==1) {
                 var wumpusImg = document.createElement("img");
-                wumpusImg.src = "img/wumpus.png";
+                wumpusImg.src = "wumpus.png";
                 div.appendChild(wumpusImg);
             }
 
             // agent
-            if(agentPosition[0]==i && agentPosition[1]==j) {
-                if(agentDirection[0]==1) agentImg.src="img/agente.png";
-                else if(agentDirection[1]==1) agentImg.src="img/agentn.png";
-                else if(agentDirection[2]==1) agentImg.src="img/agents.png";
-                else agentImg.src="img/agentw.png";
+            if(agentPosition[0]==j && agentPosition[1]==i){
+                if(agentDirection[0]==1) agentImg.src="agente.png";
+                else if(agentDirection[1]==1) agentImg.src="agentn.png";
+                else if(agentDirection[2]==1) agentImg.src="agentw.png";
+                else agentImg.src="agents.png";
                 div.appendChild(agentImg);
             }
 
@@ -104,19 +94,54 @@ function main(data) {
         table.appendChild(row);
     }
     tableContainer.appendChild(table);
+}
 
-    document.querySelector("#go-forward").addEventListener("click", function() {
-      data.res.action = 0;
-      console.log(data);
-      sendPostRequest(data);
+
+class Observer { // 상태를 갖는 객체
+
+  staticData={};
+
+  constructor() {
+    fetch('http://localhost:5000/wumpus')
+      .then(response => response.json())
+      .then(data => {
+        this.staticData = data; // GET response를 staticData에 저장
+        console.log(this.staticData);
+        main(this.staticData);
+      });
+  }
+
+  sendPostRequest(action) {
+    this.staticData.res.action=action;
+    console.log(this.staticData);
+
+    fetch('http://localhost:5000/wumpus', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.staticData)
     })
-    document.querySelector("#turn-left").addEventListener("click", function() {
-      data.res.action = 1;
-      sendPostRequest(data);
-    })
-    document.querySelector("#turn-right").addEventListener("click", function() {
-      data.res.action = 2;
-      sendPostRequest(data);
-    })
+    .then(response => response.json())
+    .then(data => {
+      this.staticData = data; // POST response를 저장
+      main(this.staticData);
+    });
+  }
 
 }
+
+const observer = new Observer();
+console.log(observer.staticData); // 생성자를 통해 객체를 생성했는데 왜 초기화 되지 않았지?
+
+document.querySelector("#go-forward").addEventListener("click", function() {
+  observer.sendPostRequest(0); // 객체에 저장된 상태를 통해 POST 요청 send.
+})
+
+document.querySelector("#turn-left").addEventListener("click", function() {
+  observer.sendPostRequest(1);
+})
+
+document.querySelector("#turn-right").addEventListener("click", function() {
+  observer.sendPostRequest(2);
+})
